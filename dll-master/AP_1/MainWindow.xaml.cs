5162fda96b0f0d41ad9893877f34df560b8f070b
Eyskil <mickael.tournie@gmail.com>
2017-05-29 16:00:13 +0200

    Add files via upload
    Dll V1.2

---
+++ dll-master/AP_1/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MyDll;
+using System.Diagnostics;
+
+using System.IO;
+using System.Security;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace AP_1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyDll.Class1 AP = new MyDll.Class1();
+
+        MyDll.Crypto CR = new MyDll.Crypto();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Atout Protect";
+
+            DirectoryInfo di = Directory.CreateDirectory(getPath() + "\\AppData\\Roaming\\Atout Protect");
+
+            
+        }
+
+        private void btnValider_Click(object sender, RoutedEventArgs e)
+        {
+            crtLic();
+
+            lblRes.Content = AP.Verification(getKey());
+
+            msgBox(AP.Verification(getKey()));
+
+            Crypto.EncryptFile(@getPath() + "\\AppData\\Roaming\\Atout Protect\\atoutprotect.lic", @getPath() + "\\AppData\\Roaming\\Atout Protect\\atoutprotectcrypt.lic", "1234512345678976");
+
+            Crypto.DecryptFile(@getPath() + "\\AppData\\Roaming\\Atout Protect\\atoutprotectcrypt.lic", @getPath() + "\\AppData\\Roaming\\Atout Protect\\atoutprotectDEcrypt.lic", "1234512345678976");
+        }
+
+        private string getKey()
+        {
+            string key = txtBoxLic1.Text + txtBoxLic2.Text + txtBoxLic3.Text + txtBoxLic4.Text;
+
+            return key;
+        }
+
+        private string getEmail()
+        {
+            string email = txtBoxEmail.Text;
+
+            return email;
+        }
+
+        private void crtLic()
+        {
+            string[] lines = { getKey(), getEmail() };
+            System.IO.File.WriteAllLines(@getPath() + "\\AppData\\Roaming\\Atout Protect\\atoutprotect.lic", lines);
+        }
+
+        private string getPath()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            return path;
+        }
+
+        private void msgBox(string bl)
+        {
+            if (bl == "true")
+            {
+                msgBoxVal();
+            }
+            else
+            {
+                msgBoxNotVal();
+            }
+        }
+
+        private void msgBoxVal()
+        {
+            MessageBox.Show("La licence est valide", "Verification");
+        }
+
+        private void msgBoxNotVal()
+        {
+            MessageBoxResult result = MessageBox.Show("Voulez vous acheter une Licence ?", "Verification", MessageBoxButton.YesNoCancel);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    MessageBox.Show("Vous allez etre rediriger vers le site de Atout Protect", "Verification");
+                    Process.Start("http://www.opisto.fr");
+                    DirectoryInfo di = Directory.CreateDirectory("C:\\AP");
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("Oh c'est pas très gentil", "Verification");
+                    break;
+                case MessageBoxResult.Cancel:
+                    MessageBox.Show("Tant pis...", "Verification");
+                    break;
+            }
+        }
+
+        private void txtBoxLic1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (txtBoxLic1.Text.Length == 4)
+            {
+                FocusManager.SetFocusedElement(this, txtBoxLic2);
+            }
+        }
+
+        private void txtBoxLic2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (txtBoxLic2.Text.Length == 4)
+            {
+                FocusManager.SetFocusedElement(this, txtBoxLic3);
+            }
+        }
+
+        private void txtBoxLic3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (txtBoxLic3.Text.Length == 4)
+            {
+                FocusManager.SetFocusedElement(this, txtBoxLic4);
+            }
+        }
+    }
+
+}
---
+++ dll-master/MyDll/Class1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+using System.IO;
+using System.Security;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace MyDll
+{
+    public class Class1
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public string Verification(string key)
+        {
+            string html;
+            string request = "http://127.0.0.1/validLicence.php?key=" + key;
+            HttpWebRequest webrequest = WebRequest.Create(request) as HttpWebRequest;
+
+            using (HttpWebResponse response = (HttpWebResponse)webrequest.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            return html;
+        }
+    }
+
+    public class Crypto
+    {
+        public static void EncryptFile(string inputFile, string outputFile, string skey)
+        {
+            try
+            {
+                using (RijndaelManaged aes = new RijndaelManaged())
+                {
+                    byte[] key = ASCIIEncoding.UTF8.GetBytes(skey);
+
+                    /* This is for demostrating purposes only.
+                     * Ideally you will want the IV key to be different from your key and you should always generate a new one for each encryption in other to achieve maximum security*/
+                    byte[] IV = ASCIIEncoding.UTF8.GetBytes(skey);
+
+                    using (FileStream fsCrypt = new FileStream(outputFile, FileMode.Create))
+                    {
+                        using (ICryptoTransform encryptor = aes.CreateEncryptor(key, IV))
+                        {
+                            using (CryptoStream cs = new CryptoStream(fsCrypt, encryptor, CryptoStreamMode.Write))
+                            {
+                                using (FileStream fsIn = new FileStream(inputFile, FileMode.Open))
+                                {
+                                    int data;
+                                    while ((data = fsIn.ReadByte()) != -1)
+                                    {
+                                        cs.WriteByte((byte)data);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // failed to encrypt file
+            }
+        }
+
+
+        public static void DecryptFile(string inputFile, string outputFile, string skey)
+        {
+            try
+            {
+                using (RijndaelManaged aes = new RijndaelManaged())
+                {
+                    byte[] key = ASCIIEncoding.UTF8.GetBytes(skey);
+
+                    /* This is for demostrating purposes only.
+                     * Ideally you will want the IV key to be different from your key and you should always generate a new one for each encryption in other to achieve maximum security*/
+                    byte[] IV = ASCIIEncoding.UTF8.GetBytes(skey);
+
+                    using (FileStream fsCrypt = new FileStream(inputFile, FileMode.Open))
+                    {
+                        using (FileStream fsOut = new FileStream(outputFile, FileMode.Create))
+                        {
+                            using (ICryptoTransform decryptor = aes.CreateDecryptor(key, IV))
+                            {
+                                using (CryptoStream cs = new CryptoStream(fsCrypt, decryptor, CryptoStreamMode.Read))
+                                {
+                                    int data;
+                                    while ((data = cs.ReadByte()) != -1)
+                                    {
+                                        fsOut.WriteByte((byte)data);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // failed to decrypt file
+            }
+        }
+
+        public static bool readInReg()
+        {
+            bool check = false;
+
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\AtoutProtect");
+
+            //if it does exist, retrieve the stored values 
+            if (key != null)
+            {
+                string Security = "";
+                string Activate = "";
+                Security = key.GetValue("security").ToString();
+                Activate = key.GetValue("Activate").ToString();
+                if (Security == "0" && Activate == "0")
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                }
+                key.Close();
+            }
+
+            return check;
+        }
+
+        public static void WriteInReg()
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\AtoutProtect");
+
+            //storing the values          
+            key.SetValue("security", "0");
+            key.SetValue("CopyRight", "ATOUTSA");
+            key.SetValue("Activate", "0");
+
+            key.Close();
+
+        }
+
+        public static void WriteInRegTry()
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\AtoutProtect");
+
+            //storing the values          
+            key.SetValue("security", "1");
+            key.SetValue("Activate", "1");
+
+            key.Close();
+
+        }
+    }
+}
